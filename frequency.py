c65f7504a1e004dd1337532146dc2280ac5975b0
AhmedZia-crypto <zia14fox@gmail.com>
2025-10-16 18:14:28 +0300

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,14 @@
+dict1 = {"codingal":3, "is":2, "best":4, "for":5, "coding":1}
+print(dict1)
+
+freq = int(input("Enter the frequency"))
+if freq == 3:
+    print("Codingal")
+elif freq == 2:
+    print("is")
+elif freq == 4:
+    print("best")
+elif freq == 5:
+    print("for")
+else:
+    print("coding")
